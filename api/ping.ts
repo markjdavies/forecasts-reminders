6d949dd10ee4305545f2e14b93e98537e5aecfbe
markjdavies <46246921+markjdavies@users.noreply.github.com>
2020-09-14 19:26:21 +0100

    ping alive

--- api/ping.ts
+++ api/ping.ts
@@ -1,7 +1,7 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 
 const ping = async (_req: NowRequest, res: NowResponse): Promise<void> => {
-    res.status(200).json('acknowledged');
+    res.status(200).json('alive');
 };
 
 export default ping;
